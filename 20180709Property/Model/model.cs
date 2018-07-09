d2be5bab81fffc00a9d2acc99c1bba20e03703ba
名宏 鄭 <free576002@gmail.com>
2018-07-09 12:05:37 +0800

    加入專案檔案。

---
+++ 20180709Property/Model/model.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _Model
+{
+    public class model
+    {
+        public string testString { get; set; }
+        public int testInt { get; set; }
+    }
+}
---
+++ 20180709Property/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Reflection;
+using _Model;
+namespace _20180709Property
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProPertiesFun<model>();
+        }
+
+        public static void ProPertiesFun<T>()
+        {
+            var GetProPertie = typeof(T).GetProperties();
+            var TModel = Activator.CreateInstance<T>();
+
+            foreach (var p in GetProPertie)
+            {
+                if (p.PropertyType == typeof(string))
+                {
+                    p.SetValue(TModel, "1234");
+                }
+                else
+                {
+                    p.SetValue(TModel, 123);
+                }
+            }
+        }
+    }
+}
